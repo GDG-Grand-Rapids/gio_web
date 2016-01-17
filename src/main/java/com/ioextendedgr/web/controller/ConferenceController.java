b237a90fd2dd0cf7919035d462122547f8f91f39
Carlus Henry <carlushenry@gmail.com>
2016-01-17 16:22:15 -0500

    enabled log4j logging.

--- src/main/java/com/ioextendedgr/web/controller/ConferenceController.java
+++ src/main/java/com/ioextendedgr/web/controller/ConferenceController.java
@@ -1,15 +1,14 @@
 package com.ioextendedgr.web.controller;
 
 import java.util.Collection;
-import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ioextendedgr.web.builder.ConferenceViewBuilder;
-import com.ioextendedgr.web.data.Conference;
 import com.ioextendedgr.web.service.ConferenceService;
 import com.ioextendedgr.web.viewDto.ConferenceSessionView;
 import com.ioextendedgr.web.viewDto.ConferenceView;
@@ -17,12 +16,13 @@ import com.ioextendedgr.web.viewDto.ConferenceView;
 @RestController
 @RequestMapping("/api")
 public class ConferenceController {
-	
+	private static final Log logger = LogFactory.getLog(ConferenceController.class);
 	@Autowired
 	private ConferenceService conferenceService;
 
     @RequestMapping("/conference")
     public Collection<ConferenceView> findAllConferences() {
+    	logger.info("Starting here!!!!");
     	return conferenceService.findAllConferences();
     }
 
